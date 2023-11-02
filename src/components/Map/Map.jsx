34e594533c36e61ff1d4e41d0d3dc29b09803f27
d3xb0t <d3xb0t@gmail.com>
2023-11-02 08:52:37 -0500

    First commit

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
 import './Map.css'
 
 async function fetchIps() {
@@ -10,19 +10,20 @@ async function fetchIps() {
 
 export const  Map = () => {
         const [ips, setIps] = useState([])
-        console.log(ips)
-        const array = [[3.4481066,-76.5671378], [3.4541254,-76.5282079], [4.6486206,-74.2726238], [6.2443677,-75.6636144], [10.9839694,-74.9004204]]
         useEffect(function() {
             fetchIps().then((fetchedIps) => setIps(fetchedIps))
         }, [])
     
     return(
-        <MapContainer center={[3.4541254,-76.5282079]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[21.201454,-39.9573199]} zoom={2} scrollWheelZoom={false}>
             <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {array.map(coordinate => (
-            <Marker key={coordinate} position={coordinate}>
-                <Popup>{coordinate}</Popup>
-            </Marker>
+            {ips.map(coordinate => (
+            <Circle key={coordinate[0]} center={coordinate[1]} radius={10000} pathOptions={{ color: coordinate[2]>0?'red':'green' }}> 
+                <Popup>
+                    {coordinate[0]}
+                </Popup>
+            </Circle>
+            
             ))}
         </MapContainer>
     )
